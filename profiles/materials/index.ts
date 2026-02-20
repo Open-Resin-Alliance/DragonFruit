f368c97536deea9f0013569460155341383f3023
PaulGD03 <paulskapczyk@gmail.com>
2026-02-20 14:46:38 +0100

    refactor(profiles): migrate profiles to dedicated folder, split json files per-manufacturer

---
+++ profiles/materials/index.ts
@@ -0,0 +1,9 @@
+import defaultMaterials from './default/materials.json';
+import concepts3dMaterials from './concepts3d/materials.json';
+
+const materialTemplates = [
+  ...defaultMaterials,
+  ...concepts3dMaterials,
+];
+
+export default materialTemplates;
