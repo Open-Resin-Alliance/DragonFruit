ca0c757cf4224038c306359f0565843c7d16866b
PaulGD03 <paulskapczyk@gmail.com>
2026-09-22 19:29:33 +0200

    style(viewcube): nudge the home button out a little

--- src/components/scene/SceneCanvas/ZUpGizmoHelper.tsx
+++ src/components/scene/SceneCanvas/ZUpGizmoHelper.tsx
@@ -178,7 +178,7 @@ function RotationArrow({
 }
 
 /** Home button sits on the bottom-right diagonal, between the right and bottom arrows. */
-const HOME_OFFSET = 0.7;
+const HOME_OFFSET = 0.76;
 const HOME_SIZE = 0.3;
 
 function HomeButton({
